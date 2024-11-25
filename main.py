751173d4afeaefcc0057f29b24737b57b27c67a2
sean00023 <sean00023mo@gmail.com>
2024-11-25 18:03:56 +0800

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from discord import app_commands
 from discord.ext import commands
 
 load_dotenv()
-DISCORD_TOKEN = os.getenv('DISCORD_TOKEN')
+DISCORD_TOKEN = os.getenv('DISCORD_TOKEN_PORK')
 
 import logging
 
