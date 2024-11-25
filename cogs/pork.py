cd583d5fb14471a527b73c0321985e62f7629d4f
sean00023 <sean00023mo@gmail.com>
2024-11-25 22:08:53 +0800

    Update pork.py

--- cogs/pork.py
+++ cogs/pork.py
@@ -57,9 +57,9 @@ class pork(commands.Cog):
             
             message_2 = self.find_reply("/reply", code, 2.0)
             if message_2["額外回應(比如影片/圖片之類的)"]:
-                embed = Embed(title="神秘訊息")
-                embed.set_video(url=message_2["額外回應(比如影片/圖片之類的)"])
-                await interaction.user.send(embed=embed)
+                # embed = Embed(title="神秘訊息")
+                # embed.set_video(url=message_2["額外回應(比如影片/圖片之類的)"])
+                await interaction.user.send(url=message_2["額外回應(比如影片/圖片之類的)"])
             await asyncio.sleep(5)    # 等待    
             await interaction.user.send(content=message_2["助手AI霸姬 回應"])
             
