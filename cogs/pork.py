55f171ff413c3623608e528f8fbe10a590a85bb1
sean00023 <sean00023mo@gmail.com>
2024-11-25 21:36:26 +0800

    1125

--- cogs/pork.py
+++ cogs/pork.py
@@ -57,7 +57,7 @@ class pork(commands.Cog):
             
             message_2 = self.find_reply("/reply", code, 2.0)
             if message_2["額外回應(比如影片/圖片之類的)"]:
-                await interaction.user.send(content=f'神秘訊息: {message_2["額外回應(比如影片/圖片之類的)"]}')
+                await interaction.user.send(content=f'[神秘訊息] ({message_2["額外回應(比如影片/圖片之類的)"]})')
             await asyncio.sleep(5)    # 等待    
             await interaction.user.send(content=message_2["助手AI霸姬 回應"])
             
