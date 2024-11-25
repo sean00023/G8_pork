c8b38c9ce5a77b16b853008f539f1635eeb70153
sean00023 <sean00023mo@gmail.com>
2024-11-25 20:14:04 +0800

    data

--- cogs/pork.py
+++ cogs/pork.py
@@ -48,35 +48,38 @@ class pork(commands.Cog):
     
         
     @app_commands.command(description='回復指令')
-    async def reply(self, interaction: Interaction, code: str = None):
-        if code == "我懂了":
+    async def reply(self, interaction: Interaction, code: str):
+        if code == "我懂了" or code == "LOOK" or code == "GAME" or code == "HAND":
             message=self.find_reply("/reply", code ,1.0)
             await interaction.user.send(content=message["助手AI霸姬 回應"])
             if message["額外回應(比如影片/圖片之類的)"]:
                 await interaction.user.send(content=message["額外回應(比如影片/圖片之類的)"])
             
             message_2 = self.find_reply("/reply", code, 2.0)
-            await asyncio.sleep(2)    # 等待    
+            await asyncio.sleep(5)    # 等待    
             await interaction.user.send(content=message_2["助手AI霸姬 回應"])
             if message_2["額外回應(比如影片/圖片之類的)"]:
                 await interaction.user.send(content=message_2["額外回應(比如影片/圖片之類的)"])
             
             message_3 = self.find_reply("/reply", code, 3.0)
-            await asyncio.sleep(2)    # 等待    
+            await asyncio.sleep(5)    # 等待    
             await interaction.user.send(content=message_3["助手AI霸姬 回應"])
             if message_3["額外回應(比如影片/圖片之類的)"]:
                 await interaction.user.send(content=message_3["額外回應(比如影片/圖片之類的)"])
-            print(message,message_2,message_3)
+            # print(message,message_2,message_3)
         else:
             message = self.find_reply("/reply", code)
-            print(message)
+            # print(message)
             
             if message:
                 await interaction.user.send(content=message["助手AI霸姬 回應"])
                 
                 if message["額外回應(比如影片/圖片之類的)"]:
                     await interaction.user.send(content=message["額外回應(比如影片/圖片之類的)"])
-     
+            else: #答錯(非以上指定的參數)
+                message = self.find_reply("/reply", "答錯(非以上指定的參數)")
+                # print(message)
+                await interaction.user.send(content=message["助手AI霸姬 回應"])
         self.record_user_action(interaction.user.id, 'reply', code)
     
     @app_commands.command(description='開始遊戲')
@@ -84,16 +87,16 @@ class pork(commands.Cog):
         message_1 = self.find_reply("/start", None ,1.0)
         message_2 = self.find_reply("/start", None ,2.0)
         message_3 = self.find_reply("/start", None ,3.0)
-        print(message_1,message_2,message_3)
+        # print(message_1,message_2,message_3)
         
         await interaction.response.send_message(f"玩家{interaction.user.name} 已經加入了遊戲")
         # 回應初始訊息
         await interaction.user.send(content=message_1["助手AI霸姬 回應"])
         await interaction.user.send(content=message_1["額外回應(比如影片/圖片之類的)"])       
-        await asyncio.sleep(2)    # 等待    
+        await asyncio.sleep(5)    # 等待    
         # 發送第二個訊息
         await interaction.user.send(content=message_2["助手AI霸姬 回應"])
-        await asyncio.sleep(2)  # 等待  
+        await asyncio.sleep(5)  # 等待  
         # 發送第三個訊息
         await interaction.user.send(content=message_3["助手AI霸姬 回應"])
         await interaction.user.send(content=message_3["額外回應(比如影片/圖片之類的)"])
